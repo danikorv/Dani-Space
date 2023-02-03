ed5fb6ef40c002e815f92fefe97fbcd5ee0a81c9
danikorv <korvd115@newschool.edu>
2023-02-02 19:54:36 -0500

    f

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-
-	var radius = 8;
-	TweenMax.staggerFromTo('.blob', 4 ,{
-		cycle: {
-			attr:function(i) {
-				var r = i*90;
-				return {
-					transform:'rotate('+r+') translate('+radius+',0.1) rotate('+(-r)+')'
-				}      
-			}
-		}  
-	},{
-		cycle: {
-			attr:function(i) {
-				var r = i*90+360;
-				return {
-					transform:'rotate('+r+') translate('+radius+',0.1) rotate('+(-r)+')'
-				}      
-			}
-		},
-		ease:Linear.easeNone,
-		repeat:-1
-	});
+var doms = document.getElementsByClassName("glowy-blob"); var
+selected = 0; for(var x =0;x<doms.length;x++) { for(var i =
+0;i<doms[x].children.length;i++) {
+doms[x].children[i].addEventListener("mousedown", function(e){
+selected = this; }) } } window.addEventListener("mouseup",
+function(e){ selected.style.animation = "bounceback 1s ease";
+selected.style.animationFillMode = "forwards"; var lala = selected;
+selected = null; setTimeout(function() { lala.style.animation = "";
+lala.style.animationFillMode = ""; lala.style.left = "";
+lala.style.top = ""; },1000) }) window.addEventListener("mousemove",
+function(e) { if(selected == null || !selected) return;
+selected.style.left = (e.clientX -
+(selected.getBoundingClientRect().width/2) - window.innerWidth/2)
++"px" selected.style.top = (e.clientY -
+(selected.getBoundingClientRect().height/2) - window.innerHeight/2 )
++"px" selected.style.animation = 0; })
